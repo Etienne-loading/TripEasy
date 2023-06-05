89df763238543e31e9bef6538d1a9df96062f521
Jean-Baptiste Bernard <jb.bernard92@gmail.com>
2023-06-05 17:04:12 +0200

    controller added

---
+++ test/controllers/tips_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class TipsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get tips_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get tips_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get tips_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get tips_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get tips_edit_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get tips_destroy_url
+    assert_response :success
+  end
+end
