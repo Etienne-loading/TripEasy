ebdd068d80315c91147cade97c432f642fa2264b
Clément Ritz <clement.ritz@orange.fr>
2023-06-15 12:20:45 +0200

    map + banner

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -12,9 +12,6 @@ export default class extends Controller {
   scroll() {
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
     const width = Math.round((window.scrollY / height) * 100)
-
-    console.log(this.barTarget);
-    console.log(width);
     this.barTarget.style.width = `${width}%`
   }
 }
